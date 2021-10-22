2231dca2ed4ae38ae5439cabf1500a5edd6de4bb
Leo Wotzak <leojwotzak@gmail.com>
2021-10-22 17:14:40 -0500

    made tenure dash wider

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -24,7 +24,7 @@ const CompanyCard = ({ frontmatter, image }) => {
         <h5 className="text-muted">{location}</h5>
         <h4 className="mt-2">{position}</h4>
         <h5 className="text-muted mt-2">
-          {startDate}-{endDate}
+          {startDate} &#8212; {endDate}
         </h5>
       </div>
     </Container>
