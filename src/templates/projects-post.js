1232092d53a54332f0e3ed64db922f89f27fea26
Leo Wotzak <leojwotzak@gmail.com>
2021-10-22 13:39:33 -0500

    added template for projects pages

--- src/templates/projects-post.js
+++ src/templates/projects-post.js
@@ -1,17 +1,35 @@
 import React from "react"
 import { graphql } from "gatsby"
 import PostTemplate from "./post-template"
-import Badge from "react-bootstrap/Badge"
+import { Badge, Container, Row, Col } from "react-bootstrap";
+import { StaticImage } from "gatsby-plugin-image";
+import Img from "gatsby-image";
 
-const SubTitle = ({ tags }) => (
-  <div className="mb-5">
-    {tags.map(tag => (
-      <Badge key={tag} pill variant="dark" className="px-3 mr-1">
-        <h5 className="text-white my-0">{tag}</h5>
-      </Badge>
-    ))}
-  </div>
+
+
+const projectTitle = ({frontmatter}) => (
+  <Row className="mt-5">
+  <Col align="center">
+  <h2>{frontmatter.title}</h2>
+  {frontmatter.tags.map(tag => (
+    <Badge pill variant="dark" className="px-2 mr-1">
+      <h5 className="text-white my-0">{tag}</h5>
+      </Badge>)
+    )}
+    <hr />
+  </Col>
+  </Row>
 )
+
+const ProjectPageLayout = ({pageContext, children}) => {
+
+  return(
+  <Container>
+    {projectTitle(pageContext)}
+    <Row><Col>{children}</Col></Row>
+  </Container>
+  )
+}
 
 
 export default ProjectPageLayout;
