93282f568e5ef793854752d2feff44b64a009d67
Leo Wotzak <leojwotzak@gmail.com>
2021-10-23 11:30:00 -0500

    created GithubLink component

--- src/components/GithubLink.js
+++ src/components/GithubLink.js
@@ -1,1 +1,36 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaGithubSquare } from "react-icons/fa";
+
+
+export default ({link}) => {
+  console.log("link: ", link)
+
+    return (
+        <Col>
+      <a href={link.link} target="_blank" rel="noopener noreferrer">
+        <FaGithubSquare className="icons file"/>
+        {/* <FontAwesomeIcon
+          icon={["fab", "fa-github"]}
+          className="icons file"
+          title="Resume"
+        /> */}
+        </a>
+        </Col>
+    )
+}
+
+
+{/* <Link to={to} style={{ textDecoration: "none" }}>
+<h2 className="mt-5">{title}</h2>
+</Link>
+
+
+<a href="../../resume.pdf" target="_blank" download>
+<FontAwesomeIcon
+  icon={["fas", "file-alt"]}
+  className="icons file"
+  title="Resume"
+/>
+</a> */}
