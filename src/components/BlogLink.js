cbd0ba18bee8a87abc06028eef38fa415435b7dd
Leo Wotzak <69174910+leowotzak@users.noreply.github.com>
2022-01-10 15:06:25 -0600

    Implement blog posts page
    Maps `gatsby-source-medium` posts into sections of component `BlogLink`. The rationale behind this is to get all blog views on Medium to maximize income/page viewership
    
    I was thinking about having all of my blog posts natively on my website but I'd rather have Medium be the primary source for $$
    
    * Add "Blog" as NavBar lijjk

--- src/components/BlogLink.js
+++ src/components/BlogLink.js
@@ -1,21 +1,49 @@
-// import React from "react"
-// import { Link } from "gatsby"
-// import Img from "gatsby-image"
-// import Card from "react-bootstrap/Card"
+import React from "react"
+import { Card, Container, Badge, Col, Row } from "react-bootstrap"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-// export default props => {
-//   return (
-//     <Card className="card-container" as={Link} to={props.to}>
-//       <Card.Img as={Img} fluid={props.featuredImage} className="h-50" />
-//       <Card.Body className="pt-3">
-//         <Card.Title>
-//           <h4>{props.title}</h4>
-//         </Card.Title>
-//         <Card.Subtitle className="mb-2 text-muted">
-//           {props.subtitle}
-//         </Card.Subtitle>
-//         <Card.Text>{props.excerpt}</Card.Text>
-//       </Card.Body>
-//     </Card>
-//   )
-// }
+export default ({ title, createdAt, virtuals }, idx) => {
+  return (
+    <Container>
+      <Row style={{ alignItems: "center" }}>
+        <Col xs={12} md={6} xl={4}>
+          <img
+            src="https://rustacean.net/assets/rustacean-flat-happy.png"
+            style={{ maxWidth: "360px" }}
+          />
+        </Col>
+        <Col fluid>
+          <Row>
+            <Col xs={12} xl={9}>
+              <h5>
+                {idx < 1 ? (
+                  <>
+                    <Badge variant="dark">New</Badge>
+                    &nbsp;
+                  </>
+                ) : (
+                  <></>
+                )}
+                {title}
+              </h5>
+            </Col>
+            <Col xs={12} xl={3}>
+              <h5>{createdAt}</h5>
+            </Col>
+          </Row>
+          <p>{virtuals.subtitle}</p>
+          {virtuals.tags.map(tag => (
+            <Badge key={tag.slug} pill variant="dark" className="px-2 mr-1">
+              {tag.name}
+            </Badge>
+          ))}
+
+          {/* {console.log(virtuals.socialRecommendsCount)}
+          {console.log(virtuals.responsesCreatedCount)}
+          {console.log(virtuals.recommends)}
+          {console.log(virtuals.publishedInCount)} */}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
---
+++ src/pages/blog.js
@@ -0,0 +1,55 @@
+import React, { useContext } from "react"
+import { graphql } from "gatsby"
+import { PageLayout, PageTitle, BlogLink } from "../components"
+import ThemeContext from "../utils/theme"
+import { Image, Row } from "react-bootstrap"
+import { SEO, Utils } from "../utils"
+import Container from "react-bootstrap/Container"
+
+export default ({ data }) => {
+  const allBlogPosts = data.allMediumPost.edges || []
+  const { dark } = useContext(ThemeContext)
+  console.log(allBlogPosts)
+
+  return (
+    <PageLayout>
+      <SEO title="Blog" />
+      <PageTitle title="Blog" />
+      <Container align="center">
+        {allBlogPosts.map(({ node }, idx) => (
+          <>
+            <section>{BlogLink(node, idx)}</section>
+            <hr />
+          </>
+        ))}
+      </Container>
+    </PageLayout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMediumPost {
+      edges {
+        node {
+          title
+          createdAt(formatString: "MMMM YYYY")
+          id
+          virtuals {
+            subtitle
+            socialRecommendsCount
+            responsesCreatedCount
+            tags {
+              slug
+              name
+              postCount
+              type
+            }
+            recommends
+            publishedInCount
+          }
+        }
+      }
+    }
+  }
+`
